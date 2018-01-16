7c40555d8355e62696103f04199065197a34e9ba
pdiazmon <pedroluis.diaz@gmail.com>
2018-01-16 10:38:39 +0100

    Initial Commit

---
+++ PDMUtils_iOS/PDMUtils_iOS.h
@@ -0,0 +1,19 @@
+//
+//  PDMUtils_iOS.h
+//  PDMUtils_iOS
+//
+//  Created by Pedro L. Diaz Montilla on 16/1/18.
+//  Copyright © 2018 Pedro L. Diaz Montilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PDMUtils_iOS.
+FOUNDATION_EXPORT double PDMUtils_iOSVersionNumber;
+
+//! Project version string for PDMUtils_iOS.
+FOUNDATION_EXPORT const unsigned char PDMUtils_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDMUtils_iOS/PublicHeader.h>
+
+
---
+++ PDMUtils_iOSTests/PDMUtils_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  PDMUtils_iOSTests.swift
+//  PDMUtils_iOSTests
+//
+//  Created by Pedro L. Diaz Montilla on 16/1/18.
+//  Copyright © 2018 Pedro L. Diaz Montilla. All rights reserved.
+//
+
+import XCTest
+@testable import PDMUtils_iOS
+
+class PDMUtils_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
